6e88561143165f47a959ecc51a4d59d17102ff36
bowets <bforsek@gmail.com>
2021-06-09 21:21:41 +0000

    fix allowed hosts

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-$0$-ow$q&6goievio^skj!&c$%0j^x$m_3iz#9%y+!p1bcgns2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bowets-todo-app.herokuapp.com']
 
 
 # Application definition
